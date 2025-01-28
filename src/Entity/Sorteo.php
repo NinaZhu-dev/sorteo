96f34a1e26b88a1ccdf2fcab75c0e4706c04f26c
Nina <ninazhurman@gmail.com>
2025-01-28 20:02:25 +0100

    proyecto sorteo loteria Once

---
+++ src/Entity/Sorteo.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SorteoRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SorteoRepository::class)]
+class Sorteo
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $fecha = null;
+
+    #[ORM\Column]
+    private ?int $numero = null;
+
+    #[ORM\Column]
+    private ?int $serie = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $premio = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFecha(): ?\DateTimeInterface
+    {
+        return $this->fecha;
+    }
+
+    public function setFecha(\DateTimeInterface $fecha): static
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    public function getNumero(): ?int
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(int $numero): static
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    public function getSerie(): ?int
+    {
+        return $this->serie;
+    }
+
+    public function setSerie(int $serie): static
+    {
+        $this->serie = $serie;
+
+        return $this;
+    }
+
+    public function getPremio(): ?int
+    {
+        return $this->premio;
+    }
+
+    public function setPremio(?int $premio): static
+    {
+        $this->premio = $premio;
+
+        return $this;
+    }
+}
---
+++ src/Form/SorteoType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sorteo;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SorteoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('fecha', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('numero')
+            ->add('serie')
+            ->add('premio')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sorteo::class,
+        ]);
+    }
+}
